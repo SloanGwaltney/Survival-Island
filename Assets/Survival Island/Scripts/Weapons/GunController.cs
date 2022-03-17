ba270de70ce1acc252a48bbba4d44adcd2bc3e69
sloangwaltney2@gmail.com <sloangwaltney2@gmail.com>
2022-03-17 16:24:07 -0400

    added action cycle logic

--- Assets/Survival Island/Scripts/Weapons/GunController.cs
+++ Assets/Survival Island/Scripts/Weapons/GunController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,10 +6,13 @@ public class GunController : MonoBehaviour
     [SerializeField] private Gun gun;
     private float roundsInClip;
     private bool isReloading;
+    private bool isActionCycling;
     private float reloadTimeElapsed;
+    private float actionCyclingTimeElapsed;
     public UnityEvent<Gun> GunFire;
     public UnityEvent<Gun> GunReloading;
     public UnityEvent<Gun> GunReloadFinished;
+    public UnityEvent<Gun> ActionCyclingStarted;
     private void Awake()
     {
         roundsInClip = gun.RoundsPerClip;
@@ -25,31 +26,52 @@ public class GunController : MonoBehaviour
     private void OnDisable()
     {
         reloadTimeElapsed = 0f;
+        isReloading = false;
+        isActionCycling = false;
     }
 
     public void PullTrigger()
     {
-        if (roundsInClip > 0 && !isReloading)
+        if (roundsInClip > 0 && !isReloading && !isActionCycling)
         {
             roundsInClip--;
             GunFire.Invoke(gun);
+            if (gun.Action.CycleTime > 0f)
+            {
+                StartActionCycle();
+                GameObject projectile = Instantiate<GameObject>(gun.ProjectilePrefab, transform.forward, transform.rotation);
+                Rigidbody rigidbody = projectile.GetComponent<Rigidbody>();
+                if (!rigidbody) Debug.Log("A gun fired a bullet without a rigidbody");
+                rigidbody?.AddForce(transform.forward * gun.ProjectileFireForce, ForceMode.Force);
+            }
         }
     }
 
     public void Reload()
     {
-        if (!isReloading)
+        if (!isReloading && !isActionCycling)
         {
             isReloading = true;
             GunReloading.Invoke(gun);
         }
     }
 
-    private void FinishReload()
+    protected void FinishReload()
     {
         isReloading = false;
         reloadTimeElapsed = 0f;
         GunReloadFinished.Invoke(gun);
+    }
+
+    protected void StartActionCycle()
+    {
+        ActionCyclingStarted.Invoke(gun);
+        isActionCycling = true;
+    }
+
+    protected void FinishActionCycle()
+    {
+        isActionCycling = false;
     }
 
     private void Update()
@@ -60,6 +82,14 @@ public class GunController : MonoBehaviour
             if (reloadTimeElapsed >= gun.ReloadTime)
             {
                 FinishReload();
+            }
+        }
+        if (isActionCycling)
+        {
+            actionCyclingTimeElapsed += Time.deltaTime;
+            if (actionCyclingTimeElapsed >= gun.Action.CycleTime)
+            {
+                FinishActionCycle();
             }
         }
     }
