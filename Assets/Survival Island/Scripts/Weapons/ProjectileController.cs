4e296ae65c23420aeead6c77aabfb0d11611bd90
SloanGwaltney <sloangwaltney2@gmail.com>
2022-03-26 08:19:41 -0400

    now spawns projectile bassed on screen center

--- Assets/Survival Island/Scripts/Weapons/ProjectileController.cs
+++ Assets/Survival Island/Scripts/Weapons/ProjectileController.cs
@@ -4,10 +4,12 @@ public class ProjectileController : MonoBehaviour
 {
     [SerializeField] private PrefabReference projectileToSpawn;
     [SerializeField] private FloatReference projectileFireForce;
+    [SerializeField] private Camera cam;
     // Called with unity events
     public void FireProjectile()
     {
-        GameObject projectile = Instantiate<GameObject>(projectileToSpawn.Value, transform.forward, transform.rotation);
+        Vector3 cameraCenter = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1));
+        GameObject projectile = Instantiate<GameObject>(projectileToSpawn.Value, cameraCenter, transform.rotation);
         Rigidbody rigidbody = projectile.GetComponent<Rigidbody>();
         if (!rigidbody) Debug.Log("A gun fired a bullet without a rigidbody");
         rigidbody?.AddForce(transform.forward * projectileFireForce.Value, ForceMode.Force);
